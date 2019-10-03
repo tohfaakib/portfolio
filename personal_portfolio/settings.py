0340c591be2f202c0c7f5b2faea863ccf97967e4
tohfaakib <tohfaakib@my.smccd.edu>
2019-10-03 12:27:42 +0600

    .

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "petergsalmo12@gmail.com"
-EMAIL_HOST_PASSWORD = 'smriti224'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
